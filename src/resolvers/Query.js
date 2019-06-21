98a20fda225ed9351259001e690b1050646dfaf5
William Okafor <wilpat456@gmail.com>
2019-06-21 23:47:37 +0100

    Implements sorting, pagination and filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,30 @@
-function feed (parent, args, context, info) {
-  return context.prisma.links();
+async function feed (parent, args, context, info) {
+  const where = args.filter ? {
+  	OR: [
+	  { description_contains: args.filter },
+	  { url_contains: args.filter }
+  	]
+  } : {}
+  // This tells prisma to include an OR where clause to the query to the database
+
+  const links = await context.prisma.links({ 
+  	where,
+  	skip: args.skip,
+  	first: args.first,
+  	orderBy: args.orderBy
+  });
+
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+    
+  return {
+  	links,
+  	count
+  }
 }
 
 function link(parent, args, context) {
